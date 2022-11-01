39172a75117a38ed2e28f62dab6979e97f06d5bd
gaveer <45834601+gaveer@users.noreply.github.com>
2022-11-01 21:35:05 +0300

    Update ExtVendor.lua

--- ExtVendor.lua
+++ ExtVendor.lua
@@ -1,7 +1,7 @@
 EXTVENDOR_DATA = {};
 EXTVENDOR = {
     Version = GetAddOnMetadata("ExtVendor", "Version"),
-    VersionID = 10900,
+    VersionID = 10902,
     ItemsPerSubpage = MERCHANT_ITEMS_PER_PAGE,
     SubpagesPerPage = 2,
     Hooks = {},
